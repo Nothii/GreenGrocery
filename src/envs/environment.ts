9a95761a37fb26250d49c90b0925b0e5117bc8a1
MR NOTHING <99800273+Nothii@users.noreply.github.com>
2024-04-24 22:42:52 +0300

    add firebase config

--- src/envs/environment.ts
+++ src/envs/environment.ts
@@ -1,1 +1,10 @@
 // Add firebase configs here
+export const firebaseConfig = {
+    apiKey: "AIzaSyCpRqNcexMFoHKGXYwtphPMXgBWz_8oX9Q",
+    authDomain: "goodfellas-green-grocery-f006c.firebaseapp.com",
+    projectId: "goodfellas-green-grocery-f006c",
+    storageBucket: "goodfellas-green-grocery-f006c.appspot.com",
+    messagingSenderId: "241448558268",
+    appId: "1:241448558268:web:c79a1a24013688ab0cbab9",
+    measurementId: "G-TK74BWYVQE"
+  };
